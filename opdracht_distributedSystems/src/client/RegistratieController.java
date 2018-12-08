5821cdc0fb2a05857df86003cc9e5cd084f226f7
thomas.bruneel <thomas.bruneel@hotmail.com>
2018-12-08 16:08:58 +0100

    registreren met een sterk passwoord is verplicht

--- opdracht_distributedSystems/src/client/RegistratieController.java
+++ opdracht_distributedSystems/src/client/RegistratieController.java
@@ -7,6 +7,9 @@ import static client.ClientMain.NoTokenCheck;
 import static client.ClientMain.openUIScreen;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 import databankServer.DatabankServerImpl;
 
@@ -46,16 +49,25 @@ public class RegistratieController {
         String gebruikersNaam=uiGebruikersnaam.getText();
         String wachtwoord= uiWachtwoord.getText();
         String herhaalwachtwoord= uiHerhaalWachtwoord.getText();
-        
+        System.out.println("registreren");
         boolean check=ClientMain.asi.controleerUniekeNaam(gebruikersNaam);
         if(check){
         	if(wachtwoord.equals(herhaalwachtwoord)){
-        		ClientMain.asi.register(gebruikersNaam, wachtwoord);
-                uiGebruikersnaam.clear();
-                uiWachtwoord.clear();
-                uiHerhaalWachtwoord.clear();
-                errorMessage.setText("registratie voltooid");
-                errorMessage.setVisible(true);
+        		//String error = null;
+        		//error=isValid(wachtwoord,error);
+        		//if(error==null){
+            		ClientMain.asi.register(gebruikersNaam, wachtwoord);
+                    uiGebruikersnaam.clear();
+                    uiWachtwoord.clear();
+                    uiHerhaalWachtwoord.clear();
+                    errorMessage.setText("registratie voltooid");
+                    errorMessage.setVisible(true);
+        		//}
+        		//else{
+        			//errorMessage.setText(error);
+                    //errorMessage.setVisible(true);
+        		//}
+
         	}
         	
         	else{
@@ -78,8 +90,39 @@ public class RegistratieController {
 
 
     }
-    
-    public void terugNaarLogin(){
+    public static String isValid(String password, String error) {
+
+        //Pattern specailCharPatten = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+        Pattern UpperCasePatten = Pattern.compile("[A-Z ]");
+        Pattern lowerCasePatten = Pattern.compile("[a-z ]");
+        Pattern digitCasePatten = Pattern.compile("[0-9 ]");
+        
+
+
+        if (password.length() < 8) {
+            error="Password lenght must have alleast 8 character !!";
+        }
+        /*
+        if (!specailCharPatten.matcher(password).find()) {
+            error="Password must have atleast one specail character !!";
+        }
+        */
+        if (!UpperCasePatten.matcher(password).find()) {
+            error="Password must have atleast one uppercase character !!";
+        }
+        if (!lowerCasePatten.matcher(password).find()) {
+            error="Password must have atleast one lowercase character !!";
+        }
+        if (!digitCasePatten.matcher(password).find()) {
+            error="Password must have atleast one digit character !!";
+        }
+        System.out.println("error "+error);
+        return error;
+
+    }
+
+
+	public void terugNaarLogin(){
     	NoTokenCheck("loginUI.fxml");
         loginLink.getScene().getWindow().hide();
 
