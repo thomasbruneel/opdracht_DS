676ed724b0a4c9697b98c5cd83ecd2019e236188
thomas.bruneel <thomas.bruneel@hotmail.com>
2018-12-08 15:49:56 +0100

    update dispatcher

--- opdracht_distributedSystems/src/dispatcher/DispathcherImpl.java
+++ opdracht_distributedSystems/src/dispatcher/DispathcherImpl.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import applicationServer.ActiveGame;
+import interfaces.AppServerInterface;
 import interfaces.DatabankServerInterface;
 import interfaces.DispatcherInterface;
 
@@ -22,6 +23,55 @@ public class DispathcherImpl extends UnicastRemoteObject implements DispatcherIn
 		appServers=new ArrayList<>();
 		databankServers=new ArrayList<>();
 	}
+	
+	
+	public void registerDatabaseServer(int port,int id) throws RemoteException{
+		Registry registry=LocateRegistry.getRegistry("localhost",port);
+		try {
+			DatabankServerInterface dsi=(DatabankServerInterface) registry.lookup("DataBankService");
+			DatabankServer dbserver=new DatabankServer(id,"localhost",port,dsi);
+			databankServers.add(dbserver);
+		} catch (NotBoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	public void registerAppServer(int port,int id) throws RemoteException{
+		Registry registry=LocateRegistry.getRegistry("localhost",port);
+		try {
+			AppServerInterface asi=(AppServerInterface) registry.lookup("DataBankService");
+			AppServer appserver=new AppServer(id,"localhost",port,asi);
+			appServers.add(appserver);
+		} catch (NotBoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	public List<AppServer> getAppServers() {
+		return appServers;
+	}
+
+	public void setAppServers(List<AppServer> appServers) {
+		this.appServers = appServers;
+	}
+
+	public List<DatabankServer> getDatabankServers() {
+		return databankServers;
+	}
+
+	public void setDatabankServers(List<DatabankServer> databankServers) {
+		this.databankServers = databankServers;
+	}
+	
+	
+	
+	
+	
 
 	
 
