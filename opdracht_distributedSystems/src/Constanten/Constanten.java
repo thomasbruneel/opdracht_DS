721b9ee44b45d55573562cd288431d5853a62cb5
Birgen Vermang <birgen.vermang@gmail.com>
2018-12-15 10:16:45 +0100

    constanten

--- opdracht_distributedSystems/src/Constanten/Constanten.java
+++ opdracht_distributedSystems/src/Constanten/Constanten.java
@@ -2,6 +2,17 @@ package Constanten;
 
 public class Constanten {
 
-    
+    public static int DISPATCHER_PORT = 6666;
+
+    public static final int AANTAL_DATABANKEN = 3;
+    public static final String DATABANK_IP = "localhost";
+    public static final int DATABANK_POORTRANGE_START = 6690;
+    public static final int DATABANK_POORTRANGE_EIND = 6700;
+
+    public static final String APPSERVER_IP = "localhost";
+    public static final int APPSERVER_POORTRANGE_START = 6707;
+    public static final int APPSERVER_POORTRANGE_EIND = 6777;       //=> max 70 appservers
+
+
 
 }
