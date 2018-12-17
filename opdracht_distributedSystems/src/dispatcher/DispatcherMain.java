7e4517f540dcefc3d794f9020e04d9418574973c
Birgen Vermang <birgen.vermang@gmail.com>
2018-12-17 13:19:06 +0100

    nullpointer?

--- opdracht_distributedSystems/src/dispatcher/DispatcherMain.java
+++ opdracht_distributedSystems/src/dispatcher/DispatcherMain.java
@@ -10,6 +10,8 @@ public class DispatcherMain extends Application {
 		@Override
 		public void start(Stage primaryStage) {
 			try {
+
+				//hier een nullpointer?
 				AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("dispatcherUI.fxml"));
 				Scene scene = new Scene(root,1074,769);
 				primaryStage.setScene(scene);
